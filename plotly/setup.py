aa809de201fdf569c776fbc0a6ecd9dcc96d1123
odeimaiz <maiz@itis.swiss>
2020-07-01 14:29:02 +0200

    version dump, cleanup

--- plotly/setup.py
+++ plotly/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'z43_styles',
   packages = ['z43_styles'],
-  version = '0.1',
+  version = '0.2',
   license='MIT',
   description = 'Z43 plotly templates',
   author = 'Odei Maiz',
@@ -15,11 +15,11 @@ setup(
     'plotly',
   ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
