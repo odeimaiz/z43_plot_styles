2ebf7b6b4106c9524b106aac06950b2a1b8a1d44
odeimaiz <maiz@itis.swiss>
2020-07-01 14:26:04 +0200

    layout dict added

--- plotly/z43_styles/z43_styles.py
+++ plotly/z43_styles/z43_styles.py
@@ -2,69 +2,73 @@ import plotly.graph_objs as go
 import plotly.io as pio
 
 pio.templates["z43_base"] = go.layout.Template(
-    font=dict(
-        size=14,
-        family="Roboto"
-    ),
-    title=dict(
-        x=0.5,
+    layout=dict(
         font=dict(
-            size=22,
+            size=14,
             family="Roboto"
         ),
-    ),
-    xaxis=dict(
         title=dict(
+            x=0.5,
             font=dict(
-                size=16,
+                size=22,
                 family="Roboto"
-            )
+            ),
         ),
-        ticks="outside"
-    ),
-    yaxis=dict(
-        title=dict(
-            font=dict(
-                size=16,
-                family="Roboto"
-            )
+        xaxis=dict(
+            title=dict(
+                font=dict(
+                    size=16,
+                    family="Roboto"
+                )
+            ),
+            ticks="outside"
         ),
-        ticks="outside"
+        yaxis=dict(
+            title=dict(
+                font=dict(
+                    size=16,
+                    family="Roboto"
+                )
+            ),
+            ticks="outside"
+        ),
     )
 )
 
 pio.templates["z43_dark"] = go.layout.Template(
-    font=dict(
-        color="#FFFFFF"
-    ),
-    plot_bgcolor="#202020",
-    paper_bgcolor="#202020",
-    xaxis=dict(
-        title=dict(
-            font=dict(
+    layout=dict(
+        font=dict(
+            color="#FFFFFF"
+        ),
+        plot_bgcolor="#202020",
+        paper_bgcolor="#202020",
+        xaxis=dict(
+            title=dict(
+                font=dict(
+                    color="#BFBFBF"
+                )
+            ),
+            tickfont=dict(
                 color="#BFBFBF"
-            )
+            ),
+            tickcolor="#BFBFBF",
+            gridcolor="#373737",
+            linecolor="#373737",
+            zerolinecolor="#373737"
         ),
-        tickfont=dict(
-            color="#BFBFBF"
-        ),
-        tickcolor="#BFBFBF",
-        gridcolor="#373737",
-        linecolor="#373737",
-        zerolinecolor="#373737"
-    ),
-    yaxis=dict(
-        title=dict(
-            font=dict(
+        yaxis=dict(
+            title=dict(
+                font=dict(
+                    color="#BFBFBF"
+                )
+            ),
+            tickfont=dict(
                 color="#BFBFBF"
-            )
-        ),
-        tickfont=dict(
-            color="#BFBFBF"
-        ),
-        tickcolor="#BFBFBF",
-        gridcolor="#373737",
-        linecolor="#373737",
-        zerolinecolor="#373737"
+            ),
+            tickcolor="#BFBFBF",
+            gridcolor="#373737",
+            linecolor="#373737",
+            zerolinecolor="#373737"
+        )
     )
 )
