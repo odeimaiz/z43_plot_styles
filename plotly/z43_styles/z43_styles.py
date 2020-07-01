4c2c0b90d054a80b6a36640580430efa4d62fb40
odeimaiz <maiz@itis.swiss>
2020-07-01 14:57:49 +0200

    light and poster styles added

--- plotly/z43_styles/z43_styles.py
+++ plotly/z43_styles/z43_styles.py
@@ -44,40 +44,133 @@ pio.templates["z43_base"] = go.layout.Template(
     )
 )
 
-pio.templates["z43_dark"] = go.layout.Template(
+pio.templates["z43_poster"] = go.layout.Template(
     layout=dict(
         font=dict(
-            color="#FFFFFF"
+            size=22,
+            family="Roboto"
         ),
-        plot_bgcolor="#202020",
-        paper_bgcolor="#202020",
+        title=dict(
+            x=0.5,
+            font=dict(
+                size=40,
+                family="Roboto"
+            ),
+        ),
         xaxis=dict(
             title=dict(
                 font=dict(
-                    color="#BFBFBF"
+                    size=24,
+                    family="Roboto"
                 )
             ),
-            tickfont=dict(
-                color="#BFBFBF"
-            ),
-            tickcolor="#BFBFBF",
-            gridcolor="#373737",
-            linecolor="#373737",
-            zerolinecolor="#373737"
+            ticks="outside"
         ),
         yaxis=dict(
             title=dict(
                 font=dict(
-                    color="#BFBFBF"
+                    size=24,
+                    family="Roboto"
+                )
+            ),
+            ticks="outside"
+        ),
+        images=[dict(
+            x=1,
+            y=0,
+            sizex=0.2,
+            sizey=0.2,
+            source="https://github.com/odeimaiz/z43_plot_styles/blob/master/imgs/z43_logo.png?raw=true",
+            opacity=0.5,
+            visible=True
+        )]
+    )
+)
+
+
+text_dark = "#FFFFFF"
+text_axes_dark = "#BFBFBF"
+bg_dark = "#202020"
+grid_dark = "#373737"
+
+pio.templates["z43_dark"] = go.layout.Template(
+    layout=dict(
+        font=dict(
+            color=text_dark
+        ),
+        plot_bgcolor=bg_dark,
+        paper_bgcolor=bg_dark,
+        xaxis=dict(
+            title=dict(
+                font=dict(
+                    color=text_axes_dark
                 )
             ),
             tickfont=dict(
-                color="#BFBFBF"
+                color=text_axes_dark
             ),
-            tickcolor="#BFBFBF",
-            gridcolor="#373737",
-            linecolor="#373737",
-            zerolinecolor="#373737"
+            tickcolor=text_axes_dark,
+            gridcolor=grid_dark,
+            linecolor=grid_dark,
+            zerolinecolor=grid_dark
+        ),
+        yaxis=dict(
+            title=dict(
+                font=dict(
+                    color=text_axes_dark
+                )
+            ),
+            tickfont=dict(
+                color=text_axes_dark
+            ),
+            tickcolor=text_axes_dark,
+            gridcolor=grid_dark,
+            linecolor=grid_dark,
+            zerolinecolor=grid_dark
+        )
+    )
+)
+
+
+text_light = "#000000"
+text_axes_light = "#404040"
+bg_light = "#DFDFDF"
+grid_light = "#C8C8C8"
+
+pio.templates["z43_light"] = go.layout.Template(
+    layout=dict(
+        font=dict(
+            color=text_light
+        ),
+        plot_bgcolor=bg_light,
+        paper_bgcolor=bg_light,
+        xaxis=dict(
+            title=dict(
+                font=dict(
+                    color=text_axes_light
+                )
+            ),
+            tickfont=dict(
+                color=text_axes_light
+            ),
+            tickcolor=text_axes_light,
+            gridcolor=grid_light,
+            linecolor=grid_light,
+            zerolinecolor=grid_light
+        ),
+        yaxis=dict(
+            title=dict(
+                font=dict(
+                    color=text_axes_light
+                )
+            ),
+            tickfont=dict(
+                color=text_axes_light
+            ),
+            tickcolor=text_axes_light,
+            gridcolor=grid_light,
+            linecolor=grid_light,
+            zerolinecolor=grid_light
         )
     )
 )
